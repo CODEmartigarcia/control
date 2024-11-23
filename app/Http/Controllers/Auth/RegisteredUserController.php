26b043d86efe3456269b56154e2ee3366800ae2f
CODEmartigarcia <martigarcia@iesmontsia.org>
2024-11-23 16:31:38 +0100

    Fix: Add DNI and Validations

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,14 +31,16 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'digits:4', 'confirmed'],
+            'dni' => ['required', 'string', 'max:10'],
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'dni' => $request->dni,
         ]);
 
         event(new Registered($user));
